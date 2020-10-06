492c7d7ffa56b9156aa10759210a6808a6d63efe
davidmolina2810 <davidmolina2810@gmail.com>
2020-10-05 23:40:14 -0500

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,13 @@
+class Dog
+
+  # method that puts Woof!
+  def bark
+    puts "Woof!"
+  end
+
+  # method that responds that the dog is sitting
+  def sit 
+    puts "The Dog is sitting"
+  end
+
+end
---
+++ lib/person.rb
@@ -0,0 +1,13 @@
+class Person
+
+  # method that puts Hello World!
+  def talk
+    puts "Hello World!"
+  end
+
+  # method to say person is walking
+  def walk 
+    puts "The Person is walking"
+  end
+
+end
